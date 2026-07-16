c848b36a983f710c82a58ce0313e1e1391bf3205
alqadhy <abdelrahmankhalidalqadhy@gmail.com>
2026-07-16 18:15:03 +0300

    spaces handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-
 import { RouterProvider } from "react-router-dom";
-
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
 import router from "./routes";
-
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000, 
+      staleTime: 5 * 60 * 1000,
       retry: 1,
       refetchOnWindowFocus: false,
     },
